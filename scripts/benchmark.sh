d33358ebd2ef909d4157c97529e955d0fb4d0264
yanglsh <yanglsh@shanghaitech.edu.cn>
2024-03-31 21:44:14 -0600

    Add script for benchmark

---
+++ scripts/benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  MU_IMPL="seq"
+else
+  MU_IMPL=$1
+fi
+
+BUILD_PREFIX=build_benchmark
+
+# rm -rf $BUILD_PREFIX 
+mkdir -p $BUILD_PREFIX
+
+cmake  -DMAKE_CXX_COMPILER=g++ \
+       -DCMAKE_CXX_FLAGS="-O3" \
+       -DMU_IMPL=$MU_IMPL \
+       -DMU_ENABLE_SINGLE=ON \
+       -B $BUILD_PREFIX/build_single
+
+cmake --build $BUILD_PREFIX/build_single
+
+$BUILD_PREFIX/build_single/bin/graupel tasks/1500k.nc
+
