e0374cf42b658799521dd3c83905f390c6bca7a2
yanglsh <yanglsh@shanghaitech.edu.cn>
2024-04-05 11:26:41 -0600

    Unroll fall speed to partial instantiation

--- core/properties/fall_speed.hpp
+++ core/properties/fall_speed.hpp
@@ -28,8 +28,33 @@ TARGET real_t fall_speed(real_t density, array_t params) {
   return params[0] * pow((density + params[2]), params[1]);
 }
 
-template <const size_t idx> TARGET real_t fall_speed(real_t density) {
-  return params[idx][0] * pow((density + params[idx][2]), params[idx][1]);
+#ifdef MU_ENABLE_OMP
+template <const size_t idx> TARGET real_t fall_speed(real_t density);
+
+template <> TARGET real_t fall_speed<0>(real_t density) {
+  return static_cast<real_t>(14.58) *
+         pow((density + static_cast<real_t>(1.0e-12)),
+             static_cast<real_t>(0.111));
 }
+
+template <> TARGET real_t fall_speed<1>(real_t density) {
+  return static_cast<real_t>(1.25) *
+         pow((density + static_cast<real_t>(1.0e-12)),
+             static_cast<real_t>(0.160));
+}
+
+template <> TARGET real_t fall_speed<2>(real_t density) {
+  return static_cast<real_t>(57.80) *
+         pow((density + static_cast<real_t>(1.0e-12)),
+             static_cast<real_t>(static_cast<real_t>(0.5) /
+                                 static_cast<real_t>(3.0)));
+}
+
+template <> TARGET real_t fall_speed<3>(real_t density) {
+  return static_cast<real_t>(12.24) *
+         pow((density + static_cast<real_t>(1.0e-08)),
+             static_cast<real_t>(0.217));
+}
+#endif
 
 } // namespace property
