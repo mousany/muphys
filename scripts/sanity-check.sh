27308c5d52bd508aeaf4346e390bc83531405c0c
yanglsh <yanglsh@shanghaitech.edu.cn>
2024-04-06 06:28:11 -0600

    Scripts for GPU diffn

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+function diffn-gpu {
+  threshold=1e-11
+
+  awk -v thr="$threshold" '{
+      all_rows=all_rows $0 "\n"; # Store all rows
+      if (NF < 10) next; # Skip non-data lines
+
+      min = $8; mean = $9; max = $10;
+
+      if ((min < -thr || min > thr) || (mean < -thr || mean > thr) || (max < -thr || max > thr)) {
+          count_differs++;
+      }
+      total++;
+  }
+
+  END {
+      if (count_differs > 0) {
+          print all_rows;
+          printf "%d out of %d records differ\n", count_differs, total;
+      }
+  }'
+}
+
 if [ -z "$1" ]; then
   MU_IMPL="seq"
 else
@@ -13,6 +36,14 @@ else
   BUILD_CXX_COMPILER=g++
 fi
 
+function check {
+  if [ $MU_IMPL == "gpu" ]; then
+    cdo infon -sub output.nc $1 | diffn-gpu
+  else
+    cdo diffn output.nc $1
+  fi
+}
+
 BUILD_PREFIX=build_sanity
 
 # rm -rf $BUILD_PREFIX 
@@ -22,33 +53,39 @@ cmake  -DCMAKE_CXX_COMPILER=$BUILD_CXX_COMPILER \
        -DCMAKE_CXX_FLAGS="-O0" \
        -DMU_IMPL=$MU_IMPL \
        -DMU_ENABLE_SINGLE=ON \
-       -B $BUILD_PREFIX/build_single
-
+       -B $BUILD_PREFIX/build_single && \
 cmake --build $BUILD_PREFIX/build_single
 
-$BUILD_PREFIX/build_single/bin/graupel tasks/dbg.nc
-cdo diffn output.nc reference_results/dbg_single.nc
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
-$BUILD_PREFIX/build_single/bin/graupel tasks/input.nc
-cdo diffn output.nc reference_results/sequential_single_output.nc
+$BUILD_PREFIX/build_single/bin/graupel tasks/dbg.nc && \
+check reference_results/dbg_single.nc
 
-$BUILD_PREFIX/build_single/bin/graupel tasks/20k.nc
-cdo diffn output.nc reference_results/sequential_single_20k.nc
+$BUILD_PREFIX/build_single/bin/graupel tasks/input.nc && \
+check reference_results/sequential_single_output.nc
+
+$BUILD_PREFIX/build_single/bin/graupel tasks/20k.nc && \
+check reference_results/sequential_single_20k.nc
 
 
 # cmake  -DCMAKE_CXX_COMPILER=$BUILD_CXX_COMPILER \
 #        -DCMAKE_CXX_FLAGS="-O0" \
 #        -DMU_IMPL=$MU_IMPL \
 #        -DMU_ENABLE_SINGLE=OFF \
-#        -B $BUILD_PREFIX/build_double
-
+#        -B $BUILD_PREFIX/build_double && \
 # cmake --build $BUILD_PREFIX/build_double
 
-# $BUILD_PREFIX/build_double/bin/graupel tasks/dbg.nc
-# cdo diffn output.nc reference_results/dbg_double.nc
+# if [ $? -ne 0 ]; then
+#   exit 1
+# fi
 
-# $BUILD_PREFIX/build_double/bin/graupel tasks/input.nc
-# cdo diffn output.nc reference_results/sequential_double_output.nc
+# $BUILD_PREFIX/build_double/bin/graupel tasks/dbg.nc && \
+# check reference_results/dbg_double.nc
 
-# $BUILD_PREFIX/build_double/bin/graupel tasks/20k.nc
-# cdo diffn output.nc reference_results/sequential_double_20k.nc
+# $BUILD_PREFIX/build_double/bin/graupel tasks/input.nc && \
+# check reference_results/sequential_double_output.nc
+
+# $BUILD_PREFIX/build_double/bin/graupel tasks/20k.nc && \
+# check reference_results/sequential_double_20k.nc
