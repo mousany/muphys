075c192c7001d30df9e5d1efc2a34853baadbf92
Linshu Yang <b382789@levante6.lvt.dkrz.de>
2024-04-17 13:41:38 +0200

    Add my-build.sh, my-run.sh

---
+++ scripts/my-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#!/bin/bash
+
+#SBATCH --account=ka1273
+#SBATCH --job-name=scc
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --output=%x.%j.log
+#SBATCH --exclusive
+#SBATCH --time=00:10:00
+
+ulimit -s unlimited
+ulimit -c 0
+
+. scripts/levante-setup.sh nvidia gpu
+
+export LD_LIBRARY_PATH=/sw/spack-levante/nvhpc-23.7-xasprs/Linux_x86_64/23.7/profilers/Nsight_Systems/host-linux-x64/:/sw/spack-levante/nvhpc-23.7-xasprs/Linux_x86_64/23.7/cuda/lib64
+build/bin/graupel $1
